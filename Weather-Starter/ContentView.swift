98cc9514a484503f0f8a07fe544d074a209da1aa
César González <cesar.gonzalez@ciberexperis.es>
2021-02-24 12:26:38 +0100

    Initial Commit

---
+++ Weather-Starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-Starter
+//
+//  Created by César González Palomino on 24/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather-Starter/Weather_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_StarterApp.swift
+//  Weather-Starter
+//
+//  Created by César González Palomino on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Weather_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
